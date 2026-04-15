79ee6d2ab302649200be44c687e2b41ec03aa7e5
rohithpamarthi-git <ssaipamarthi@@gmail.com>
2026-04-15 21:42:27 +0530

    Configure Neon PostgreSQL database using dj-database-url

--- mentalhealth_project/settings.py
+++ mentalhealth_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -79,10 +80,9 @@ WSGI_APPLICATION = 'mentalhealth_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
